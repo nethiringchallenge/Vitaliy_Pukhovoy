a04cf41cc292a9a0e1cb7309866eb91d8fd06c9d
vitaliypukhovoy <vitaliypukhovoy@gmail.com>
2018-08-22 18:34:35 +0300

    unit test

---
+++ Office_space/Unit_Office_Space/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit_Office_Space
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
